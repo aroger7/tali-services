75bd41ee75e871ccbfad2936f2ada64a69226992
Adam Rogers <adam@adamrogers.ca>
2021-09-04 16:02:49 -0400

    add config

--- count-update-service/update-player-counts-function/config/index.js
+++ count-update-service/update-player-counts-function/config/index.js
@@ -1,4 +1,12 @@
-exports.DB_SECRET_NAME = process.env.DB_SECRET_NAME || 'TallyDbDevCredentialsSecret';
-exports.GET_PLAYER_COUNTS_FUNCTION_NAME = process.env.GET_PLAYER_COUNTS_FUNCTION_NAME || 'dev-get-batch-counts-function';
-exports.NUM_UPDATE_LAMBDAS = 200;
-exports.REQS_PER_SECOND_MAX = 30;
+const local = require('./local.js');
+const development = require('./development.js');
+
+const getConfig = () => {
+  if (process.env.IS_OFFLINE || process.env.IS_LOCAL) {
+    return local;
+  }
+
+  return development;
+}
+
+module.exports = getConfig();
