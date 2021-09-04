e757771a901a3a53cf687ee1113a245802d5ebe5
Adam Rogers <adam@adamrogers.ca>
2021-09-04 16:35:56 -0400

    add env vars

--- count-update-service/update-player-counts-function/config/development.js
+++ count-update-service/update-player-counts-function/config/development.js
@@ -1,4 +1,4 @@
-exports.DB_SECRET_NAME = process.env.DB_SECRET_NAME || 'TallyDbDevCredentialsSecret';
-exports.GET_PLAYER_COUNTS_FUNCTION_NAME = process.env.GET_PLAYER_COUNTS_FUNCTION_NAME || 'dev-get-batch-counts-function';
+exports.DB_SECRET_NAME = process.env.DB_SECRET_NAME;
+exports.GET_PLAYER_COUNTS_FUNCTION_NAME = process.env.GET_PLAYER_COUNTS_FUNCTION_NAME;
 exports.NUM_UPDATE_LAMBDAS = 200;
 exports.REQS_PER_SECOND_MAX = 30;
