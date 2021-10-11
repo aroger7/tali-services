67c31ea8c6ab6fb906bebd2d4ce96e7a234e02c2
Adam Rogers <adam@adamrogers.ca>
2021-10-11 16:28:53 -0400

    refactor batch count function

--- count-update-service/get-batch-counts-function/index.js
+++ count-update-service/get-batch-counts-function/index.js
@@ -1,43 +1,44 @@
-/* Amplify Params - DO NOT EDIT
-	ENV
-	REGION
-Amplify Params - DO NOT EDIT */
 'use strict';
 const axios = require('axios');
+const getArrayChunks = require('../util/getArrayChunks');
 
-module.exports.handler = (event, context, callback) => {
+module.exports.handler = async (event, context) => {
   const { apps, reqsPerSecond = 20 } = event;
   const queued = apps.concat();
-  const inProgress = [];
-  const completed = [];
-  const intervalId = setInterval(() => {
-    if (queued.length === 0 && inProgress.length === 0) {
-      clearInterval(intervalId);
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({ playerCounts: completed })
-      });
+  try { 
+    const tasks = getArrayChunks(queued, reqsPerSecond)
+      .map((appGroup, i) => new Promise((resolve) => {
+        setTimeout(async () => {
+          const groupCounts = await getCountsForAppGroup(appGroup);
+          resolve(groupCounts);
+        }, 1000 * i);
+      }));
+
+    const playerCounts = (await Promise.all(tasks)).flat();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ playerCounts })
     }
-
-    if (queued.length > 0) {
-      const newGroup = queued.splice(0, reqsPerSecond);
-      inProgress.push(...newGroup);
-
-      newGroup.map(app => {
-        return getPlayerCount(app.appid)
-          .then((count) => {
-            inProgress.splice(inProgress.findIndex(ip => ip.appid === app.appid), 1)
-            completed.push({ ...app, count });
-          })
-          .catch((err) => {
-            console.error(`Couldn't get ${app.appid} player count: ${err.message}`);
-            inProgress.splice(inProgress.findIndex(ip => ip.appid === app.appid), 1)
-            completed.push({ ...app, error: err, errorStatus: err.response.status });
-          })
-      });
-
+  }
+  catch (err) {
+    console.error('batch count function error: ', err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err)
     }
-  }, 1000);
+  }
+};
+
+const getCountsForAppGroup = async (appGroup) => {
+  const tasks = appGroup.map(async app => {
+    try {
+      const count = await getPlayerCount;
+      return { ...app, count };
+    } catch (err) {
+      return { ...app, errorStatus: err?.response?.status };
+    }
+  });
+  return Promise.all(tasks);
 };
 
 const getPlayerCount = async (appid) => {
@@ -51,8 +52,8 @@ const getPlayerCount = async (appid) => {
   } else {
     const url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1';
     const params = { appid };
-    const res = await axios.get(url, { params });
-    return res.data.response.player_count >= 0
+    const res = await axios.get(url, { params, timeout: 2000 });
+    return res.data?.response?.player_count >= 0
       ? res.data.response.player_count
       : null;
   }
